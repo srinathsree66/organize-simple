9613bf9dcd3f1c429f8ffdc4942cbd4f10a49d52
srinathsree66 <srinathsree122@gmail.com>
2024-02-26 13:02:09 +0530

    feat: 🎸 add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Organize Simple API')
+    .setDescription(
+      'Organize Simple is an API that allows you to organize your data in a way easy to use understand with power of large language model',
+    )
+    .setVersion('1.0')
+    .addTag('organize-simple')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // app.enableVersioning({
+  //   type: VersioningType.URI,
+  // });
   await app.listen(3000);
 }
 bootstrap();
